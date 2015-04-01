079cc0b451e546a1015a61049aa9939a69d629d7
pamus <pamus2k@gmail.com>
2015-04-01 18:39:17 -0400

    define resource type params to be called multiple times

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,15 @@
 class pam_policies {
-  case $operatingsystem {
-    'RedHat', 'CentOS' { 
-      if $::operatingsystemmajrelease == 6 {
-        include rh6
+  case $::operatingsystem {
+    'RedHat', 'CentOS': { 
+      if $::operatingsystemmajrelease > 5 {
+	params { 'system-auth': } 
+	params { 'password-auth': } 
+      } else {
+	params { 'system-auth': } 
       }
+    }
+    default: {
+      notice("$operatingsystem is  not currently supported for pam_policies management")
     }
   }
 }
